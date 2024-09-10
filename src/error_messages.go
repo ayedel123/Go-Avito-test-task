10dcbebc493bf6f956160c50b8ce21f79ebba114
willifer <willifer@student.21-school.ru>
2024-09-11 00:27:05 +0300

    tender status func not finished

--- src/error_messages.go
+++ src/error_messages.go
@@ -1,5 +1,7 @@
 package main
 
+import "net/http"
+
 const (
 	ErrMessageWrongUser        = "Incorrect username or user does not exist."
 	ErrMessageNoPermission     = "User does not have permission for this organization."
@@ -7,3 +9,19 @@ const (
 	ErrMessageWrongRequest     = "The request format or parameters are incorrect."
 	ErrMessageMethodNotAllowed = "Method not allowed"
 )
+
+func sendHttpErr(w http.ResponseWriter, status int) {
+	switch status {
+	case http.StatusBadRequest:
+		http.Error(w, ErrMessageWrongRequest, status)
+	case http.StatusUnauthorized:
+		http.Error(w, ErrMessageWrongUser, status)
+	case http.StatusForbidden:
+		http.Error(w, ErrMessageNoPermission, status)
+	case http.StatusMethodNotAllowed:
+		http.Error(w, ErrMessageMethodNotAllowed, status)
+	default:
+		http.Error(w, ErrMessageServer, status)
+	}
+
+}
--- src/tenders.go
+++ src/tenders.go
@@ -9,6 +9,112 @@ import (
 	"time"
 )
 
+func createTenderDataToTender(req CreateTenderData, id, user_id, version int, created_at time.Time) *Tender {
+	return &Tender{
+		ID:          id,
+		Name:        req.Name,
+		Description: req.Description,
+		Status:      req.Status,
+		ServiceType: req.ServiceType,
+		AuthorID:    user_id,
+		Version:     version,
+		CreatedAt:   created_at,
+	}
+}
+
+type CreateTenderData struct {
+	Name            string `json:"name"`
+	Description     string `json:"description"`
+	ServiceType     string `json:"serviceType"`
+	Status          string `json:"status"`
+	OrganizationID  int    `json:"organizationId"`
+	CreatorUsername string `json:"creatorUsername"`
+}
+
+func atoi(s string) (int, int) {
+
+	if num, err := strconv.Atoi(s); err == nil && num >= 0 {
+		return num, 200
+	}
+	return 0, 400
+}
+
+func sqlErrToStatus(err error, err_status int) int {
+	status := 200
+	if err != nil {
+		if err == sql.ErrNoRows {
+			status = err_status
+		} else {
+			status = 500
+		}
+
+	}
+	return status
+}
+
+func getLimitOffsetFromRequest(r *http.Request) (int, int, int) {
+	s_limit := r.URL.Query().Get("limit")
+	s_offset := r.URL.Query().Get("offset")
+
+	var limit, offset int
+
+	if s_limit == "" {
+		s_limit = "5"
+	}
+
+	if s_offset == "" {
+		s_offset = "0"
+	}
+	status := 200
+	limit, status = atoi(s_limit)
+	if status == 200 {
+		offset, status = atoi(s_offset)
+	}
+	return limit, offset, status
+
+}
+
+func getIntFromRequest(r *http.Request, default_val int, param_name string) (int, int) {
+	s_param := r.URL.Query().Get(param_name)
+	if default_val != -1 && s_param == "" {
+		s_param = strconv.Itoa(default_val)
+	}
+	param, status := atoi(s_param)
+	return param, status
+
+}
+
+func getUserNameFromRequest(r *http.Request) string {
+	username := r.URL.Query().Get("username")
+	return username
+}
+
+func isUserInOrganization(db *sql.DB, user_id, organization_id int) int {
+
+	query := `
+        SELECT orgr.user_id 
+        FROM organization_responsible orgr 
+        WHERE orgr.user_id = $1 AND orgr.organization_id = $2
+		LIMIT 1
+    `
+	err := db.QueryRow(query, user_id, organization_id).Scan(&user_id)
+
+	return sqlErrToStatus(err, 403)
+}
+
+func getUserId(db *sql.DB, user_name string) (int, int) {
+	var id int
+
+	query := `
+        SELECT e.id 
+        FROM  employee e WHERE e.username = $1
+		LIMIT 1
+    `
+	err := db.QueryRow(query, user_name).Scan(&id)
+	log.Println("name ", user_name, " err", err)
+	return id, sqlErrToStatus(err, 401)
+}
+
 func getTenders(db *sql.DB, limit, offset int) ([]Tender, error) {
 	query := `
 	SELECT id, name, description, status, service_type, author_id, version, created_at 
@@ -54,67 +160,6 @@ func tendersHandler(db *sql.DB) http.HandlerFunc {
 		}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(tenders)
-	}
-}
-
-type CreateTenderData struct {
-	Name            string `json:"name"`
-	Description     string `json:"description"`
-	ServiceType     string `json:"serviceType"`
-	Status          string `json:"status"`
-	OrganizationID  int    `json:"organizationId"`
-	CreatorUsername string `json:"creatorUsername"`
-}
-
-func sqlErrToStatus(err error, err_status int) int {
-	status := 200
-	if err != nil {
-		if err == sql.ErrNoRows {
-			status = err_status
-		} else {
-			status = 500
-		}
-
-	}
-	return status
-}
-
-func isUserInOrganization(db *sql.DB, user_id, organization_id int) int {
-
-	query := `
-        SELECT orgr.user_id 
-        FROM organization_responsible orgr 
-        WHERE orgr.user_id = $1 AND orgr.organization_id = $2
-		LIMIT 1
-    `
-	err := db.QueryRow(query, user_id, organization_id).Scan(&user_id)
-
-	return sqlErrToStatus(err, 403)
-}
-
-func getUserId(db *sql.DB, user_name string) (int, int) {
-	var id int
-
-	query := `
-        SELECT e.id 
-        FROM  employee e WHERE e.username = $1
-		LIMIT 1
-    `
-	err := db.QueryRow(query, user_name).Scan(&id)
-	log.Println("name ", user_name, " err", err)
-	return id, sqlErrToStatus(err, 401)
-}
-
-func createTenderDataToTender(req CreateTenderData, id, user_id, version int, created_at time.Time) *Tender {
-	return &Tender{
-		ID:          id,
-		Name:        req.Name,
-		Description: req.Description,
-		Status:      req.Status,
-		ServiceType: req.ServiceType,
-		AuthorID:    user_id,
-		Version:     version,
-		CreatedAt:   created_at,
 	}
 }
 
@@ -179,33 +224,6 @@ func newTenderHandler(db *sql.DB) http.HandlerFunc {
 
 }
 
-func getLimitOffsetFromRequest(r *http.Request) (int, int, int) {
-	s_limit := r.URL.Query().Get("limit")
-	s_offset := r.URL.Query().Get("offset")
-
-	var limit, offset int
-
-	if s_limit == "" {
-		s_limit = "5"
-	}
-
-	if s_offset == "" {
-		s_offset = "0"
-	}
-	status := 200
-	limit, status = atoi(s_limit)
-	if status == 200 {
-		offset, status = atoi(s_offset)
-	}
-	return limit, offset, status
-
-}
-
-func getUserNameFromRequest(r *http.Request) string {
-	username := r.URL.Query().Get("username")
-	return username
-}
-
 func getUserTenders(db *sql.DB, user_id, limit, offset int) ([]Tender, int) {
 	query := `
 	SELECT id, name, description, status, service_type, author_id, version, created_at
@@ -265,26 +283,41 @@ func myTendersHandler(db *sql.DB) http.HandlerFunc {
 	}
 }
 
-func atoi(s string) (int, int) {
+func getTenderStatus(tender_id, user_id int) {
 
-	if num, err := strconv.Atoi(s); err == nil && num >= 0 {
-		return num, 200
-	}
-	return 0, 400
 }
 
-func sendHttpErr(w http.ResponseWriter, status int) {
-	switch status {
-	case http.StatusBadRequest:
-		http.Error(w, ErrMessageWrongRequest, status)
-	case http.StatusUnauthorized:
-		http.Error(w, ErrMessageWrongUser, status)
-	case http.StatusForbidden:
-		http.Error(w, ErrMessageNoPermission, status)
-	case http.StatusMethodNotAllowed:
-		http.Error(w, ErrMessageMethodNotAllowed, status)
-	default:
-		http.Error(w, ErrMessageServer, status)
+func handleGetTenderStatus(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	tender_i, res_status := getIntFromRequest(r, -1, "tenderId")
+	if res_status != 200 {
+		sendHttpErr(w, res_status)
+		return
+	}
+	user_name := getUserNameFromRequest(r)
+	user_id, res_status := getUserId(db, user_name)
+	if res_status != 200 {
+		sendHttpErr(w, res_status)
+		return
 	}
 
+	getTenderStatus(tender_i, user_id)
+
+}
+
+func statusTendersHandler(db *sql.DB) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+		if r.Method != http.MethodGet {
+			sendHttpErr(w, http.StatusMethodNotAllowed)
+			return
+		} else if r.Method == http.MethodGet {
+			handleGetTenderStatus(db, w, r)
+
+		} else if r.Method == http.MethodPut {
+
+		}
+
+	}
 }
